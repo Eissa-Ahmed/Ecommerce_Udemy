8c0166e45f7304907175ae016ea9dddfe2980558
Essa <essaahmeed00@gmail.com>
2024-07-27 21:01:42 +0300

    Add project files.

---
+++ Ecommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
---
+++ Ecommerce.API/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
