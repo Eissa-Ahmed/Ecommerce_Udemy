eddbaf75103a967dd501bd616d68a2db1f6a49c3
Essa Ahmed <essa1998ahmed@gmail.com>
2024-07-29 15:51:27 +0300

    00

---
+++ Ecommerce.Data/Entities/Images.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Data.Entities;
+
+public class Images
+{
+    public string Name { get; set; } = null!;
+    public double Size { get; set; }
+    public string extension => Path.GetExtension(Name);
+    public string ProductId { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+}
--- Ecommerce.Data/Entities/Product.cs
+++ Ecommerce.Data/Entities/Product.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Ecommerce.Data.Entities;
 
-namespace Ecommerce.Data.Entities
+public class Product
 {
-    internal class Product
+    public Product()
     {
+        Id = Guid.NewGuid().ToString();
+        CreatedDate = DateTime.UtcNow;
+        Images = new List<Images>();
     }
+    public string Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string MainImage { get; set; } = null!;
+    public int quantity { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public ICollection<Images> Images { get; set; }
 }
