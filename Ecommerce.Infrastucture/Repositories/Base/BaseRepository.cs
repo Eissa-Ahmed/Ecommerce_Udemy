a7368432069073fbbdbc0120980abaefe4bdd914
Essa Ahmed <essa1998ahmed@gmail.com>
2024-07-31 05:08:03 +0300

    00

--- Ecommerce.Infrastucture/Repositories/Base/BaseRepository.cs
+++ Ecommerce.Infrastucture/Repositories/Base/BaseRepository.cs
@@ -11,15 +11,15 @@ public class BaseRepository<T> : IBaseRepository<T> where T : class
     public async Task<IEnumerable<T>> GetAllAsync(int take = 0, int skip = 0, Expression<Func<T, bool>>? filter = null)
     {
         return filter is null
-                    ? await _context.Set<T>().Skip(skip).Take(take).ToListAsync()
-                    : await _context.Set<T>().Where(filter).Skip(skip).Take(take).ToListAsync();
+                    ? await _context.Set<T>().AsNoTracking().Skip(skip).Take(take).ToListAsync()
+                    : await _context.Set<T>().AsNoTracking().Where(filter).Skip(skip).Take(take).ToListAsync();
     }
 
     public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
     {
         return filter is null
-                  ? await _context.Set<T>().ToListAsync()
-                  : await _context.Set<T>().Where(filter).ToListAsync();
+                  ? await _context.Set<T>().AsNoTracking().ToListAsync()
+                  : await _context.Set<T>().AsNoTracking().Where(filter).ToListAsync();
     }
 
 }
